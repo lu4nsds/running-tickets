6a6694fe1b0a107a99383b959b41ad838ab90498
Pedro H G Aguiar <pedrohenrykes@gmail.com>
2026-03-07 14:43:56 -0300

    chore: Atualiza registry repo da imagem api production

--- scripts/build-and-push-api.sh
+++ scripts/build-and-push-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build --no-cache -f api/Dockerfile.prod -t running-tickets-api:latest ./api \
-    && docker tag running-tickets-api:latest lu4nsds/running-tickets-api:latest \
-    && docker push lu4nsds/running-tickets-api:latest
+    && docker tag running-tickets-api:latest pedrohenrykes/running-tickets-api:latest \
+    && docker push pedrohenrykes/running-tickets-api:latest
