b69c72a13a2e4d73e04d7189e3b938e25a1dae32
Pedro H G Aguiar <pedrohenrykes@gmail.com>
2026-04-04 02:18:39 -0300

    chore: Ajusta os script de deploy para informar uuid como nome das imagem

--- scripts/build-and-push-api.sh
+++ scripts/build-and-push-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --no-cache -f api/Dockerfile.prod -t running-tickets-api:latest ./api \
-    && docker tag running-tickets-api:latest pedrohenrykes/running-tickets-api:latest \
-    && docker push pedrohenrykes/running-tickets-api:latest
+docker build --no-cache -f api/Dockerfile.prod -t 2ac5a56d-49dd-445b-a71d-65808ffa5f19:latest ./api \
+    && docker tag 2ac5a56d-49dd-445b-a71d-65808ffa5f19:latest pedrohenrykes/2ac5a56d-49dd-445b-a71d-65808ffa5f19:latest \
+    && docker push pedrohenrykes/2ac5a56d-49dd-445b-a71d-65808ffa5f19:latest
