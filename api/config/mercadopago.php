f6ada25d3aaa09ce316b695c5eb76358a5b893b5
Pedro H G Aguiar <pedrohenrykes@gmail.com>
2026-05-16 13:45:01 -0300

    chore: Remove variáveis de ambiente não utilizadas pela aplicação

--- api/config/mercadopago.php
+++ api/config/mercadopago.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-    'access_token' => env('MERCADOPAGO_ACCESS_TOKEN'),
-    'public_key' => env('MERCADOPAGO_PUBLIC_KEY'),
+    'access_token' => env('MERCADOPAGO_ACCESS_TOKEN', ''),
+    'public_key' => env('MERCADOPAGO_PUBLIC_KEY', ''),
     'api_url' => env('MERCADOPAGO_API_URL', 'https://api.mercadopago.com'),
-    'sandbox_mode' => env('MERCADOPAGO_SANDBOX_MODE', true),
 ];
